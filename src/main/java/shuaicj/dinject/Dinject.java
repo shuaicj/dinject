4d4d806db7d747cfe00e3a7f6fa5852830259f0c
shuaicj <shuaicj@gmail.com>
2018-04-28 18:35:55 +0800

    dependency parse

--- src/main/java/shuaicj/dinject/Dinject.java
+++ src/main/java/shuaicj/dinject/Dinject.java
@@ -1,52 +1,98 @@
 package shuaicj.dinject;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Executable;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 /**
- * The main class of dinject.
+ * The main class create dinject.
  *
  * @author shuaicj 2018/04/12
  */
 public final class Dinject {
 
-    private final Map<Id<?>, Meta> metas = null;
+    private final Map<Id<?>, Meta> metas = new ConcurrentHashMap<>();
     private final Map<Id<?>, Object> singletons = new ConcurrentHashMap<>();
 
-    public static Dinject of(Class<?>... moduleClasses) {
-        return new Dinject(Arrays.asList(moduleClasses));
+    public static Dinject create(Class<?>... moduleClasses) {
+        return new Dinject(new HashSet<>(Arrays.asList(moduleClasses)));
     }
 
-    public static Dinject of(Iterable<Class<?>> moduleClasses) {
-        return new Dinject(moduleClasses);
+    public static Dinject create(Collection<Class<?>> moduleClasses) {
+        return new Dinject(new HashSet<>(moduleClasses));
     }
 
-    private Dinject(Iterable<Class<?>> moduleClasses) {
-        // instance of Dinject itself is supposed to be a singleton
+    private Dinject(Collection<Class<?>> moduleClasses) {
+        // instance create Dinject itself is supposed to be a singleton
         singletons.put(new Id<>(Dinject.class), this);
 
-        // init metas
-        Map<Id<?>, Meta> metas = new HashMap<>();
-        checkModules(metas, moduleClasses);
-        checkDependencies(metas);
+        parseModules(moduleClasses);
+        generateDependencies();
+        metas.forEach((id, meta) -> validateDependencies(id, new LinkedHashSet<>()));
     }
 
     public <T> T instance(Class<T> clazz) {
+        return instance(clazz, null);
+    }
+
+    public <T> T instance(Class<T> clazz, String name) {
+        Id<T> id = new Id<>(clazz, name);
+        Object obj = singletons.get(id);
+        if (obj != null) {
+            return (T) obj;
+        }
+        Meta meta = metas.get(id);
+        if (meta != null) {
+            if (meta.singleton) {
+                synchronized (this) {
+                    obj = singletons.get(id);
+                    if (obj == null) {
+                    }
+                }
+            }
+        }
         return null;
     }
 
-    private void checkModules(Map<Id<?>, Meta> metas, Iterable<Class<?>> moduleClasses) {
+    private Object instance(Class<?> clazz, Meta meta) {
+        Object[] params = meta.params
+                .stream()
+                .map(p -> {
+                    if (p.provider) {
+                        return (Provider) () -> instance(p.id.clazz, metas.get(p.id));
+                    } else {
+                        return instance(p.id.clazz, metas.get(p.id));
+                    }
+                })
+                .collect(Collectors.toList())
+                .toArray();
+
+        if (meta.executable instanceof Constructor) {
+            return instantiateByConstructor(clazz, (Constructor) meta.executable, params);
+        }
+        return null;
+    }
+
+    private void parseModules(Iterable<Class<?>> moduleClasses) {
         for (Class<?> clazz : moduleClasses) {
             Constructor<?> ctor;
             try {
@@ -56,36 +102,50 @@ public final class Dinject {
             }
             ctor.setAccessible(true);
 
-            Object module;
-            try {
-                module = ctor.newInstance();
-            } catch (Exception e) {
-                throw new DinjectException("cannot instantiate " + clazz, e);
-            }
+            Object module = instantiateByConstructor(clazz, ctor);
 
             for (Method m : clazz.getDeclaredMethods()) {
                 if (m.isAnnotationPresent(Provides.class)) {
                     Class<?> t = m.getReturnType();
                     String n = m.isAnnotationPresent(Named.class) ? m.getAnnotation(Named.class).value() : null;
                     boolean s = m.isAnnotationPresent(Singleton.class) || t.isAnnotationPresent(Singleton.class);
-                    if (metas.putIfAbsent(new Id<>(t, n), new Meta(s, m, module)) != null) {
-                        throw new DinjectException("multiple @Provides for " + t + (n == null ? "" : " named " + n));
+                    Id<?> id = new Id<>(t, n);
+                    if (metas.putIfAbsent(id, new Meta(s, m, getParams(m), module)) != null) {
+                        throw new DinjectException("multiple @Provides for " + id);
                     }
                 }
             }
         }
     }
 
-    private void checkDependencies(Map<Id<?>, Meta> metas) {
+    private void generateDependencies() {
+        // ensure all dependencies constructable
         Queue<Id<?>> q = new LinkedList<>(metas.keySet());
         while (!q.isEmpty()) {
             Id<?> id = q.poll();
             Meta meta = metas.get(id);
-            if (meta == null) {
-                Constructor c = getConstructor(id.clazz);
-                meta = new Meta(c.isAnnotationPresent(Singleton.class), c);
-                metas.put(id, meta);
+            for (Param<?> p : meta.params) {
+                if (!metas.containsKey(p.id)) {
+                    if (p.id.name != null) {
+                        throw new DinjectException("no @Provides for " + p);
+                    }
+                    boolean s = p.id.clazz.isAnnotationPresent(Singleton.class);
+                    Constructor c = getConstructor(p.id.clazz);
+                    metas.put(p.id, new Meta(s, c, getParams(c)));
+                    q.offer(p.id);
+                }
             }
+        }
+    }
+
+    private void validateDependencies(Id<?> id, LinkedHashSet<Id<?>> path) {
+        // ensure no circular dependency
+        if (path.contains(id)) {
+            throw new DinjectException("circular dependency " + pathToString(id, path));
+        }
+        path.add(id);
+        for (Param<?> p : metas.get(id).params) {
+            validateDependencies(p.id, path);
         }
     }
 
@@ -111,6 +171,54 @@ public final class Dinject {
         ctor = ctor == null ? none : ctor;
         ctor.setAccessible(true);
         return ctor;
+    }
+
+    private Object instantiateByConstructor(Class<?> clazz, Constructor ctor, Object... params) {
+        try {
+            return ctor.newInstance(params);
+        } catch (Exception e) {
+            throw new DinjectException("cannot instantiate " + clazz + " by constructor", e);
+        }
+    }
+
+    private List<Param<?>> getParams(Executable executable) {
+        List<Param<?>> rt = new ArrayList<>();
+        Class<?>[] pt = executable.getParameterTypes();
+        Type[] gt = executable.getGenericParameterTypes();
+        Annotation[][] pa = executable.getParameterAnnotations();
+        for (int i = 0; i < pt.length; i++) {
+            boolean p = pt[i].equals(Provider.class);
+            Class<?> t = p ? (Class<?>) ((ParameterizedType) gt[i]).getActualTypeArguments()[0] : pt[i];
+            String n = null;
+            for (Annotation a : pa[i]) {
+                if (a instanceof Named) {
+                    n = ((Named) a).value();
+                    break;
+                }
+            }
+            rt.add(new Param<>(new Id<>(t, n), p));
+        }
+        return rt;
+    }
+
+    private String pathToString(Id<?> id, LinkedHashSet<Id<?>> path) {
+        StringBuilder s = new StringBuilder("{ ");
+        boolean found = false;
+        for (Id<?> d : path) {
+            if (found) {
+                s.append(" -> ");
+                s.append(d);
+                continue;
+            }
+            if (d.equals(id)) {
+                found = true;
+                s.append(d);
+            }
+        }
+        s.append(" -> ");
+        s.append(id);
+        s.append(" }");
+        return s.toString();
     }
 
     private static final class Id<T> {
@@ -139,21 +247,39 @@ public final class Dinject {
         public int hashCode() {
             return Objects.hash(clazz, name);
         }
+
+        @Override
+        public String toString() {
+            return clazz + (name == null ? "" : ":" + name);
+        }
+    }
+
+    private static final class Param<T> {
+
+        private final Id<T> id;
+        private final boolean provider;
+
+        Param(Id<T> id, boolean provider) {
+            this.id = id;
+            this.provider = provider;
+        }
     }
 
     private static final class Meta {
 
         private final boolean singleton;
-        private final Executable type;
+        private final Executable executable;
+        private final List<Param<?>> params;
         private final Object module;
 
-        Meta(boolean singleton, Executable type) {
-            this(singleton, type, null);
+        Meta(boolean singleton, Executable ctor, List<Param<?>> params) {
+            this(singleton, ctor, params, null);
         }
 
-        Meta(boolean singleton, Executable type, Object module) {
+        Meta(boolean singleton, Executable method, List<Param<?>> params, Object module) {
             this.singleton = singleton;
-            this.type = type;
+            this.executable = method;
+            this.params = params;
             this.module = module;
         }
     }
--- src/test/java/shuaicj/dinject/DinjectTest.java
+++ src/test/java/shuaicj/dinject/DinjectTest.java
@@ -2,8 +2,6 @@ package shuaicj.dinject;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import javax.inject.Inject;
-
 import org.junit.Test;
 
 /**
@@ -15,7 +13,7 @@ public class DinjectTest {
 
     // @Test
     // public void example1() {
-    //     Dinject dinject = Dinject.of();
+    //     Dinject dinject = Dinject.create();
     //     Example1.A a1 = dinject.instance(Example1.A.class);
     //     Example1.A a2 = dinject.instance(Example1.A.class);
     //     assertThat(a1).isNotNull();
@@ -25,7 +23,7 @@ public class DinjectTest {
     //
     // @Test
     // public void example2() {
-    //     Dinject dinject = Dinject.of();
+    //     Dinject dinject = Dinject.create();
     //     Example2.A a1 = dinject.instance(Example2.A.class);
     //     Example2.A a2 = dinject.instance(Example2.A.class);
     //     assertThat(a1).isNotNull();
@@ -35,7 +33,7 @@ public class DinjectTest {
     //
     // @Test
     // public void example3() {
-    //     Dinject dinject = Dinject.of();
+    //     Dinject dinject = Dinject.create();
     //     Example3.A a = dinject.instance(Example3.A.class);
     //     assertThat(a).isNotNull();
     //     assertThat(a.b).isNotNull();
@@ -43,28 +41,28 @@ public class DinjectTest {
     //
     // @Test
     // public void example4() {
-    //     Dinject dinject = Dinject.of();
+    //     Dinject dinject = Dinject.create();
     //     Example4.A a = dinject.instance(Example4.A.class);
     //     assertThat(a.b).isNotNull();
     // }
     //
     // @Test
     // public void example5() {
-    //     Dinject dinject = Dinject.of(Example5.Mod.class);
+    //     Dinject dinject = Dinject.create(Example5.Mod.class);
     //     Example5.I i = dinject.instance(Example5.I.class);
     //     assertThat(i).isNotNull();
     // }
     //
     // @Test
     // public void example6() {
-    //     Dinject dinject = Dinject.of(Example6.Mod.class);
+    //     Dinject dinject = Dinject.create(Example6.Mod.class);
     //     Example6.A a = dinject.instance(Example6.A.class);
     //     assertThat(a.b.name).isEqualTo("b2");
     // }
 
     @Test
     public void moduleDoesNotHaveNoArgConstructor() {
-        assertThatThrownBy(() -> Dinject.of(ModuleDoesNotHaveNoArgConstructor.class))
+        assertThatThrownBy(() -> Dinject.create(ModuleDoesNotHaveNoArgConstructor.class))
                 .isInstanceOf(DinjectException.class)
                 .hasMessageStartingWith("no-arg constructor required for module");
     }
@@ -76,7 +74,7 @@ public class DinjectTest {
 
     @Test
     public void multipleProvides() {
-        assertThatThrownBy(() -> Dinject.of(MultipleProvides.class))
+        assertThatThrownBy(() -> Dinject.create(MultipleProvides.class))
                 .isInstanceOf(DinjectException.class)
                 .hasMessageStartingWith("multiple @Provides for");
     }
@@ -92,7 +90,7 @@ public class DinjectTest {
 
     @Test
     public void multipleInjectConstructors() {
-        // assertThatThrownBy(() -> Dinject.of(MultipleInjectConstructors.class))
+        // assertThatThrownBy(() -> Dinject.create(MultipleInjectConstructors.class))
         //         .isInstanceOf(DinjectException.class)
         //         .hasMessageStartingWith("multiple @Provides for");
     }
